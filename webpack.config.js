2c43d4574d0ddf10f9a2daf84910c6fde2ad5b36
Nick Johnson <nickjohnson.dev@gmail.com>
2017-01-26 16:32:32 -0600

    Switched to ts-loader. Added vendor.ts file. Moved index.html directly into src. Cleaned ts/webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,20 +4,18 @@ const webpack = require('webpack');
 
 module.exports = {
   devServer: {
-    contentBase: './src/public',
+    contentBase: './dist',
     historyApiFallback: true,
     stats: 'minimal',
   },
-  devtool: 'eval-source-map',
   entry: {
     'app': './src/main.ts',
   },
   module: {
     rules: [
-      { test: /\.json$/, loader: 'json-loader' },
       {
         test: /\.ts$/,
-        loader: 'awesome-typescript-loader?inlineSourceMap=true&sourceMap=false',
+        loader: 'ts-loader?inlineSourceMap=true&sourceMap=false',
         exclude: /node_modules\/(?!(ng2-.+))/,
       },
     ],
@@ -25,25 +23,20 @@ module.exports = {
   output: {
     chunkFilename: '[id].chunk.js',
     filename: 'js/[name].js',
-    path: root('dist'),
+    path: path.join(__dirname, 'dist'),
     publicPath: 'http://localhost:8080/',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/public/index.html',
+      template: './src/index.html',
       chunksSortMode: 'dependency',
     }),
     new webpack.ContextReplacementPlugin(
       /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
-      root('./src')
+      './src'
     ),
   ],
   resolve: {
-    extensions: ['.ts', '.js', '.json', '.css', '.scss', '.html'],
+    extensions: ['.ts', '.js'],
   },
 };
-
-function root(args) {
-  const separatedArgs = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [__dirname].concat(separatedArgs));
-}
